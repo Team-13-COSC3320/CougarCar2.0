d2b6e63711810ae82ed9f963a281fd41ed13ddbf
Robert Hines <47727369+RobertHines45@users.noreply.github.com>
2020-04-12 16:36:27 -0500

    Order table

---
+++ RazorPagesTutorial/Pages/Orders/OrderTable.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesTutorial
+{
+    public class OrderTableModel : PageModel
+    {
+        private readonly RazorPagesTutorial.Data.RazorPagesTutorialContext _context;
+
+        public OrderTableModel(RazorPagesTutorial.Data.RazorPagesTutorialContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Orders> Orders { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Orders = await _context.Orders.ToListAsync();
+        }
+
+    }
+}
