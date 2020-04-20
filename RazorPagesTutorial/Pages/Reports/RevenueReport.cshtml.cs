a84f6b5f4814e9941eb1ef6dd5ccdaaa926b5c4a
Robert Hines <47727369+RobertHines45@users.noreply.github.com>
2020-04-20 03:09:54 -0500

    Both data reports, need to make review report better

---
+++ RazorPagesTutorial/Pages/Reports/RevenueReport.cshtml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Data;
+
+
+namespace RazorPagesTutorial
+{
+    public class RevenueReportModel : PageModel
+    {
+        private readonly RazorPagesTutorialContext _context;
+
+
+        public RevenueReportModel(RazorPagesTutorialContext context)
+        {
+            _context = context;
+        }
+        
+       
+        [BindProperty]
+        public DataSet dataSet { get; set; }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (HttpContext.Session.Get("Id") != null)
+            {
+                byte[] str = HttpContext.Session.Get("Id");
+                string ID = Encoding.UTF8.GetString(str, 0, str.Length);
+                ViewData["Userid"] = ID;
+                //Console.Out.Write(ViewData["Userid"]);
+            }
+            if (HttpContext.Session.Get("Role") != null)
+            {
+                byte[] str = HttpContext.Session.Get("Role");
+                string Role = Encoding.UTF8.GetString(str, 0, str.Length);
+                ViewData["UserRole"] = Role;
+        
+            }
+
+            SqlConnection con = new SqlConnection("Data Source=sql5053.site4now.net;User ID=DB_A573D4_team13_admin;Password=Team13shop;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            cmd = new SqlCommand("RevenueReport", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Date1", SqlDbType.DateTime).Value = DateTime.Now;
+
+            da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            dataSet = ds;
+            con.Close();
+
+            Console.Out.Write(dataSet.Tables[0].Columns[0].ToString());
+
+            return Page();
+        }
+
+        /* SqlConnection con = new SqlConnection("YourConnection String");
+             SqlCommand cmd = new SqlCommand();
+             SqlDataAdapter da = new SqlDataAdapter();
+             DataSet ds = new DataSet();
+             cmd = new SqlCommand("RevenueReport", con);
+             cmd.CommandType = CommandType.StoredProcedure;
+             cmd.Parameters.Add("@Date1", SqlDbType.Da).Value = DateTime.Now;
+
+             da = new SqlDataAdapter(cmd);
+             da.Fill(ds);
+             da.Fill(dataSet);
+             con.Close();
+         */
+
+
+    }
+}
---
+++ RazorPagesTutorial/Pages/Reports/ReviewReport.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTutorial.Models;
+using Microsoft.AspNetCore.Http;
+using LibraryData.Models;
+using RazorPagesTutorial.Data;
+
+namespace WebApplication3.Pages.Reports
+{
+    public class ReviewReportModel : PageModel
+    {
+        private readonly RazorPagesTutorialContext _context;
+
+
+        public ReviewReportModel(RazorPagesTutorialContext context)
+        {
+            _context = context;
+        }
+
+        public int star, currentUserID;
+
+        public IList<USER> USERS { get; set; }
+
+        public IList<Review> REVIEW { get; set; }
+
+        public IList<Product> PRODUCTS { get; set; }
+
+        public async Task OnGetAsync(int? id)
+        {
+            
+            USERS = await _context.USERS.ToListAsync();
+            REVIEW = await _context.Review.ToListAsync();
+            PRODUCTS = await _context.Product.ToListAsync();
+
+            for (int i = 0; i < USERS.Count(); i++)
+            {
+                //Msg1 = "In";
+
+                //ID;s validation
+                if (USERS[i].U_ID == id)
+                {
+                    currentUserID = USERS[i].U_ID - 1;//-1 because ID start from 1, but list is start from 0
+                }
+            }
+
+
+
+
+        }
+
+        public async Task OnPostAsync()
+        {
+            USERS = await _context.USERS.ToListAsync();
+            REVIEW = await _context.Review.ToListAsync();
+            PRODUCTS = await _context.Product.ToListAsync();
+            star = int.Parse( Request.Form["starSelected"]);
+        }
+    }
+}
