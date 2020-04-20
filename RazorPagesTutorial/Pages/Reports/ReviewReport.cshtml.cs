5e8318905502400f37f463e4960b4478e02cfdd6
alonsomunoz <60762994+alonsomunoz@users.noreply.github.com>
2020-04-20 14:37:49 -0500

    Added Quang's Stuff

--- RazorPagesTutorial/Pages/Reports/ReviewReport.cshtml.cs
+++ RazorPagesTutorial/Pages/Reports/ReviewReport.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,7 +11,7 @@ using Microsoft.AspNetCore.Http;
 using LibraryData.Models;
 using RazorPagesTutorial.Data;
 
-namespace WebApplication3.Pages.Reports
+namespace RazorPagesTutorial.Pages.Reports
 {
     public class ReviewReportModel : PageModel
     {
@@ -22,7 +23,7 @@ namespace WebApplication3.Pages.Reports
             _context = context;
         }
 
-        public int star, currentUserID;
+        public int star, currentUserID, count;
 
         public IList<USER> USERS { get; set; }
 
@@ -32,11 +33,24 @@ namespace WebApplication3.Pages.Reports
 
         public async Task OnGetAsync(int? id)
         {
-            
+            if (HttpContext.Session.Get("Id") != null)
+            {
+                byte[] str = HttpContext.Session.Get("Id");
+                string ID = Encoding.UTF8.GetString(str, 0, str.Length);
+                ViewData["Userid"] = ID;
+                //Console.Out.Write(ViewData["Userid"]);
+            }
+            if (HttpContext.Session.Get("Role") != null)
+            {
+                byte[] str = HttpContext.Session.Get("Role");
+                string Role = Encoding.UTF8.GetString(str, 0, str.Length);
+                ViewData["UserRole"] = Role;
+
+            }
             USERS = await _context.USERS.ToListAsync();
             REVIEW = await _context.Review.ToListAsync();
             PRODUCTS = await _context.Product.ToListAsync();
-
+            count = REVIEW.Count();
             for (int i = 0; i < USERS.Count(); i++)
             {
                 //Msg1 = "In";
@@ -47,18 +61,36 @@ namespace WebApplication3.Pages.Reports
                     currentUserID = USERS[i].U_ID - 1;//-1 because ID start from 1, but list is start from 0
                 }
             }
-
-
-
-
         }
 
         public async Task OnPostAsync()
         {
+            if (HttpContext.Session.Get("Id") != null)
+            {
+                byte[] str = HttpContext.Session.Get("Id");
+                string ID = Encoding.UTF8.GetString(str, 0, str.Length);
+                ViewData["Userid"] = ID;
+                //Console.Out.Write(ViewData["Userid"]);
+            }
+            if (HttpContext.Session.Get("Role") != null)
+            {
+                byte[] str = HttpContext.Session.Get("Role");
+                string Role = Encoding.UTF8.GetString(str, 0, str.Length);
+                ViewData["UserRole"] = Role;
+
+            }
             USERS = await _context.USERS.ToListAsync();
             REVIEW = await _context.Review.ToListAsync();
             PRODUCTS = await _context.Product.ToListAsync();
             star = int.Parse( Request.Form["starSelected"]);
+            count = 0;
+            foreach (Review i in REVIEW)
+            {
+                if (i.R_Star == star || star == 0)
+                {
+                    count += 1;
+                }
+            }
         }
     }
 }
