f7647de512e371137d6164712830cd1fa5e6a344
alonsomunoz <60762994+alonsomunoz@users.noreply.github.com>
2020-04-20 23:59:13 -0500

    Fixed some image errors and positioning errors

--- RazorPagesTutorial/Pages/Index.cshtml.cs
+++ RazorPagesTutorial/Pages/Index.cshtml.cs
@@ -94,9 +94,6 @@ namespace RazorPagesTutorial
                 HttpContext.Session.SetString("Index", test.ToString());
                 current_image = imagefiles[test];
                 validReviews = _context.GetReviewsOnProduct(product_ids[test]);
-                Console.SetOut(new DebugTextWriter());
-                Console.WriteLine(product_ids[test]);
-                Console.WriteLine(imagefiles[test]);
                 review_pid = test;
             }
             else if (side.Contains("Left"))
@@ -110,9 +107,6 @@ namespace RazorPagesTutorial
                 }
                 HttpContext.Session.SetString("Index", test.ToString());
                 current_image = imagefiles[test];
-                Console.SetOut(new DebugTextWriter());
-                Console.WriteLine(product_ids[test]);
-                Console.WriteLine(imagefiles[test]);
                 validReviews = _context.GetReviewsOnProduct(product_ids[test]);
                 review_pid = test;
             }
